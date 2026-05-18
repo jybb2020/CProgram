523a516bc1e56326c11791aa4445615ffc037efc
Teigan <jybb2020@gmail.com>
2026-05-18 12:52:47 +0530

    changes initial to upload to github

---
+++ Test.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GitlearningApp
+{
+  class Program
+{
+static void Main(string[] args)
+{
+console.WriteLine("Project 1:C# and git integration Successfully");
+Console.ReadLine();
+}
+}
+}
